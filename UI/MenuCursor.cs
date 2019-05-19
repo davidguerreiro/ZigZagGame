2e90370ca864b948ea58f3e6fa8b4d95429a7515
davidguerreiro <me@davidguerreiro.com>
2019-05-19 21:29:03 +0100

    complete cursor animation for main menu

---
+++ UI/MenuCursor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCursor : MonoBehaviour
+{
+    public float speed = 30f;                           // Cursor static animation speed.
+    public float amplitude = 20f;                       // Static animation amplitude.
+    private float originPosition;                       // Static animation origin position.
+    private float maxPosition;                          // Static animation maximun position.
+    private bool inAnimation;                           // Animation Flag - wheter the cursor is in an animation coroutine or not.
+    private AudioSource audioSource;                    // Audio source component.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // trigger static cursor floating movement.
+        if ( ! inAnimation ) {
+            StartCoroutine( StaticCursorAnimation() );
+        }
+
+        // testing
+        if ( Input.GetKeyDown( "c" ) ) {
+            ReproduceSelectableSound();
+        }
+    }
+
+    /// <summary>
+    /// Initialise cursor class.
+    /// </summary>
+    private void Init() {
+        // calculate origin position and max position for static animation.
+        originPosition = transform.localPosition.x;
+        maxPosition = originPosition + amplitude;
+
+        // get audio source component.
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Reproduce change selectable
+    /// sound
+    /// </summary>
+    public void ReproduceSelectableSound() {
+        audioSource.Play();
+    }
+
+    /// <summary>
+    /// Display static cursor floating
+    /// animation.
+    /// </summary>
+    public IEnumerator StaticCursorAnimation() {
+        inAnimation = true;
+
+        while ( transform.localPosition.x < maxPosition ) {
+            transform.localPosition = new Vector2( transform.localPosition.x + ( speed * Time.deltaTime ), transform.localPosition.y );
+            yield return null;
+        }
+
+        // fix variations in the movement.
+        transform.localPosition = new Vector2( maxPosition, transform.localPosition.y );
+
+        while ( transform.localPosition.x > originPosition ) {
+            transform.localPosition = new Vector2( transform.localPosition.x- ( speed * Time.deltaTime ), transform.localPosition.y );
+            yield return null;
+        }
+
+        // fix variations in the movement.
+        transform.localPosition = new Vector2( originPosition, transform.localPosition.y );
+
+        inAnimation = false;
+    }
+}
---
+++ UI/MenuSelectables.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MenuSelectables : MonoBehaviour
+{
+    public GameObject cursor;                           // Selectable Cursor.
+    private MenuCursor cursorClass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Initialise class.
+    /// </summary>
+    private void Init() {
+        
+        // get selectable cursor class.
+        if ( cursor != null ) {
+            cursorClass = cursor.GetComponent<MenuCursor>();
+        }
+    }
+
+    /// <summary>
+    /// Set this item selectable.
+    /// </summary>
+    private void SetItemSelectable() {
+        GameObject[] cursors = GameObject.FindGameObjectsWithTag( "menu_cursor");
+    }
+}
