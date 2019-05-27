1c303e0742b2128e081b3f9c2e5431e657457b80
davidguerreiro <me@davidguerreiro.com>
2019-05-27 21:49:55 +0100

    add baisc public attributes to uimanager

--- UI/UIManager.cs
+++ UI/UIManager.cs
@@ -6,5 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    
+    public GameObject TopBar;                       // UI top bar component.
+    public GameObject scorePanel;                   // UI score panel component.
+    public GameObject startText;                    // UI start text component.
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        
+    }
 }
