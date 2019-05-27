497d70dc0854e44fb6e76e9711cd19be90c841fc
davidguerreiro <me@davidguerreiro.com>
2019-05-27 22:18:00 +0100

    add score logic class

---
+++ UI/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text scoreText;                     // Score text component.
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    /// <summary>
+    /// Update score.
+    /// </summary>
+    /// <param name="amount">int - how much the score is updated</param>
+    public void UpdateScore( int amount ) {
+        int currentScore = int.Parse( scoreText.text );
+        currentScore += amount;
+
+        scoreText.text = currentScore.ToString();
+    }
+
+    /// <summary>
+    /// Initialise class method.
+    /// </summary>
+    private void Init() {
+        scoreText = GetComponent<Text>();
+    }
+}
