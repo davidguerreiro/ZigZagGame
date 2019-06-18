b0901f5f8558e34002e1b945b743cd31affce388
davidguerreiro <me@davidguerreiro.com>
2019-06-18 01:00:12 +0100

    add grass animation script

---
+++ Models/Decorative/Forest/Grass.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    public bool animated = true;                                // Wheter the grass is animated.
+    public bool frontDirection = true;                          // Initial rotation direction.
+    private float amplitude = 3f;                               // Rotation movement amplitude.
+    private float speed = 0.8f;                                   // Animation speed.
+    private float max;                                          // Rotation max value.                  
+    private float min;                                          // Rotation min value.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( animated ) {
+            Init();
+            StartCoroutine( RotateGrass() );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Rotate grass to give wind effect.
+    /// </summary>
+    private IEnumerator RotateGrass() {
+        float grades;
+        Quaternion q = transform.rotation;
+        
+        while ( animated ) {
+            grades = ( frontDirection ) ? 1 : - 1;
+
+            transform.Rotate( grades, 0, 0 );
+            Debug.Log( transform.rotation.x );
+
+            // check if rotation direction needs to change.
+            if ( transform.rotation.x > 0.001f ) {
+                frontDirection = false;
+                //yield return new WaitForSeconds( 0.1f );
+            } else if ( transform.rotation.x < - 0.001f ) {
+                frontDirection = true;
+                //yield return new WaitForSeconds( 0.1f );
+            }
+
+            yield return new WaitForSeconds( 0.3f );
+            yield return null;
+            
+        }
+        
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+        /// calculate max and min for rotation.
+        max = transform.rotation.eulerAngles.x + amplitude;
+        min = transform.rotation.x - amplitude;
+    }
+}
---
+++ Player/PlayerAnimations.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+
+    public static PlayerAnimations instance;                           // Static instance of this class.
+    public string status = "standard";                                 // Current player status. Player status defines the state in which the player is. Different states trigger animations.
+    private bool inAnimatio = false;                                   // Whether the player is in an async event for animation or not.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( StandardAnimation() );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update player status.
+    /// This will also trigger the animation associated
+    /// with this new status, if there is any.
+    /// </summary>
+    /// <param name="newStatus">string - new status</param>
+    public void UpdateStatus( string newStatus ) {
+        status = newStatus;
+    }
+
+    /// <summary>
+    /// Standard animation. The ball floates
+    /// in the air and slides smoothly.false It also
+    /// has normal eyes and an smile in the face.
+    /// </summary>
+    public IEnumerator StandardAnimation() {
+        float speed = 10f;
+        float movementAmplitude = 3f;
+        float gradesAmplitude = 10f;
+
+        string direction = "up";
+
+        while ( status == "standard" ) {
+            Quaternion q = transform.rotation;
+            // Debug.Log( transform.rotation.x );
+            transform.Rotate( 1f, 0, 0 );
+            yield return null;
+        }
+    }
+}
