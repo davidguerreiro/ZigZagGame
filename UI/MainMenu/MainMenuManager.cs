1ae7cf01103fdb98860410bd4c862afa4d1a1d07
davidguerreiro <me@davidguerreiro.com>
2019-07-14 18:21:58 +0100

    add methods to control menu items in main menu manager

--- UI/MainMenu/MainMenuManager.cs
+++ UI/MainMenu/MainMenuManager.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public static MainMenuManager instance;                    // Create public instance for this manager to be called from other scripts.
+    public static MainMenuManager instance;       // Create public instance for this manager to be called from other scripts.
     public Text descriptionTitle;                 // Game mode description title.
     public Text description;                      // Game mode description.
+    public Text maintTitle;                       // Main title text component.
+    public GameObject pressSpaceBar;              // Press space bar text component.
+    public GameObject mainMenu;                   // Main menu UI panel gameObject.
+    public GameObject descriptionPanel;           // Game option description UI panel gameObject.
+    public AudioClip startGameSound;              // Start game sound clip.
+    public SceneCover sceneCover;                 // Scene cover class component to make screen transition smoother.
+    private float fadeSpeed = 0.8f;               // Main title fade in speed.
+    private float movementSpeed = 500f;           // Menu appaerance speed for animation.
+    private AudioSource audio;                    // Audio Source component.
+
+
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -22,13 +34,21 @@ public class MainMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+        // get audio component.
+        audio = GetComponent<AudioSource>();
     }
 
     /// <summary>
@@ -50,4 +70,62 @@ public class MainMenuManager : MonoBehaviour
             description.text = text;
         }
     }
+
+    /// <summary>
+    /// Display main title text
+    /// in the scene.
+    /// </summary>
+    public IEnumerator DisplayMainTitle() {
+        while ( maintTitle.color.a < 1f ) {
+            maintTitle.color = new Color( maintTitle.color.r, maintTitle.color.g, maintTitle.color.b, maintTitle.color.a + ( fadeSpeed * Time.deltaTime ) );
+            yield return null;
+        }
+
+        // display press space bar text.
+        ToggleSpaceBarText( true );
+    }
+
+    /// <summary>
+    /// Display / hide press bar
+    /// button to open menu.
+    /// </summary>
+    /// <param name="state">bool - new state for the gameObject</param>
+    public void ToggleSpaceBarText( bool state ) {
+        pressSpaceBar.SetActive( state );
+    }
+
+    /// <summary>
+    /// Display main menu in
+    /// the scene.
+    /// </summary>
+    private IEnumerator DisplayMainMenu() {
+        float toMove = 1507f;                   // Final position for the main menu in the screen.
+        mainMenu.SetActive( true );
+
+        while ( mainMenu.transform.position.x > toMove ) {
+            mainMenu.transform.position = new Vector2( mainMenu.transform.position.x - ( movementSpeed * Time.deltaTime ), mainMenu.transform.position.y );
+            yield return null;
+        }
+
+        // fix variants in the position after the animation.
+        mainMenu.transform.position =  new Vector2( toMove, mainMenu.transform.position.y );
+    }
+
+    /// <summary>
+    /// Display description bar 
+    /// panel in the scene.
+    /// </summary>
+    private IEnumerator displayDescriptionBar() {
+        float toMove = 59.7002f;
+        
+        while (descriptionPanel.transform.position.y < toMove ) {
+            descriptionPanel.transform.position = new Vector2( descriptionPanel.transform.position.x, descriptionPanel.transform.position.y + ( movementSpeed * Time.deltaTime ) );
+            yield return null;
+        }
+
+        // fix variatns in the position after the animation.
+        descriptionPanel.transform.position = new Vector2( descriptionPanel.transform.position.x, toMove );
+    }
+
+
 }
---
+++ UI/MainMenu/SceneCover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneCover : MonoBehaviour
+{
+    public float speed = 0.8f;                                  // Fade in / out speed.
+    private Image panel;                                        // Panel component image.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+        // get image component for fade in/out animations.
+        panel = GetComponent<Image>();
+    }
+
+    /// <summary>
+    /// Fade in scene cover.
+    /// </summary>
+    public IEnumerator FadeIn() {
+        while ( panel.color.a < 1f ) {
+            panel.color = new Color( panel.color.r, panel.color.g, panel.color.b, panel.color.a + ( speed * Time.deltaTime ) );
+            yield return null;
+        }
+
+        // fix variants in the animation.
+        panel.color = new Color( panel.color.r, panel.color.g, panel.color.b, 1f );
+    }
+
+    /// <summary>
+    /// Fade out scene cover.
+    /// </summary>
+    public IEnumerator FadeOut() {
+        while ( panel.color.a > 0f ) {
+            panel.color = new Color( panel.color.r, panel.color.g, panel.color.b, panel.color.a - ( speed * Time.deltaTime ) );
+            yield return null;
+        }
+
+        // fix variants in the animation.
+        panel.color = new Color( panel.color.r, panel.color.g, panel.color.b, 0f );
+    }
+}
