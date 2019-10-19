88c9f11b470276d88a8f3cfe6f6c73860eadf80b
davidguerreiro <me@davidguerreiro.com>
2019-10-19 14:45:43 +0100

    add random sky color in the main level scene

---
+++ GameLevel/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        // set main camera sky color.
+        Utils.instance.SetSkyColor();
+    }
+}
