99f7cf051685175ab98bdc0ae368f8699b230ae4
davidguerreiro <me@davidguerreiro.com>
2019-11-02 12:44:35 +0000

    complete scripts to display and hide elements in the secondary splashscreen

---
+++ UI/SplashScreen/SplashScreen2/SplashImageComponent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashImageComponent : MonoBehaviour {
+    public string type = "";                                        // Image type. It can be 'piskel' or 'unity'. This is used to set the display animation.
+    private Animation animation;                                    // Animation component reference.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Display image animation.
+    /// Animation used is based
+    /// on image type.
+    /// </summary>
+    /// <returns>void</returns>
+    public void DisplayImage() {
+        string animationName = "";
+
+        switch ( this.type ) {
+            case "piskel":
+                animationName = "DisplayPiskelLogo";
+                break;
+            case "unity":
+                animationName = "DisplayUnityLogo";
+                break;
+            default:
+                animationName = "";
+                break;
+        }
+
+        if ( animationName != "" ) {
+            Utils.instance.TriggerAnimation( animation, animationName );
+        }
+    }
+
+    /// <summary>
+    /// Hide image animation.
+    /// </summary>
+    /// <returns>void</returns>
+    public void HideImage() {
+        Utils.instance.TriggerAnimation( animation, "HideImage" );
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    /// <returns>void</returns>
+    private void Init() {
+
+        // get animatio component reference.
+        animation = GetComponent<Animation>();
+    }
+}
---
+++ UI/SplashScreen/SplashScreen2/SplashTextComponent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashTextComponent : MonoBehaviour {
+    public string type;                                         // Splash screen text type. It can be 'upper', 'main' or 'content'. Used to check which animation has to be played.
+    private Animation animation;                                // Animaiton component reference.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Display text animation.
+    /// Animation used varies depending
+    /// of the text type.
+    /// </summary>
+    /// <returns>void</returns>
+    public void DisplayText() {
+        string animationName = "";
+
+        switch ( this.type ) {
+            case "upper":
+                animationName = "DisplayUpperText";
+                break;
+            case "main":
+                animationName = "DisplayMainText";
+                break;
+            case "content":
+                animationName = "DisplayContentText";
+                break;
+            default:
+                animationName = "";
+                break;
+        }
+
+        if ( animationName != "" ) {
+            Utils.instance.TriggerAnimation( animation, animationName );
+        }
+    }
+
+    /// <summary>
+    /// Hide text animation.
+    /// </summary>
+    /// <returns>void</returns>
+    public void HideText() {
+        Utils.instance.TriggerAnimation( animation, "HideText" );
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    /// <returns>void</returns>
+    private void Init() {
+        
+        // get animation component.
+        animation = GetComponent<Animation>();
+    }
+}
