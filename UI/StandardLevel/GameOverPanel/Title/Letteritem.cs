92c601a4ced10573cd4532956bdda5937d76da9c
davidguerreiro <me@davidguerreiro.com>
2019-12-26 12:19:55 +0000

    complete gameOver title wrapper component and letterItem component scripts

---
+++ UI/StandardLevel/GameOverPanel/Title/Letteritem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letteritem : MonoBehaviour {
+
+    private Animation animation;                        // Animation component reference.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Display letter item.
+    /// </summary>
+    /// <returns>void</returns>
+    public void Display() {
+        Utils.instance.TriggerAnimation( animation, "Display" );
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    /// <returns>void</returns>
+    private void Init() {
+
+        // get animation component.
+        animation = GetComponent<Animation>();
+    }
+
+
+}
---
+++ UI/StandardLevel/GameOverPanel/Title/TitleWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleWrapper : MonoBehaviour {
+
+    public Letteritem[] letterItems = new Letteritem[8];        // Letter items array - used to display game over animation.
+    private bool completed;                                     // Flag to control whether the title display animation has been completed.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Get animation completed
+    /// status.
+    /// </summary>
+    /// <returns>void</returns>
+    public bool IsCompleted() {
+        return this.completed;
+    }
+
+    /// <summary>
+    /// Display game over title
+    /// animation.
+    /// </summary>
+    /// <returns>IEnumerator</returns>
+    public IEnumerator DisplayTitleAnimation() {
+        float gap = .1f;
+
+        foreach ( Letteritem item in letterItems ) {
+            item.Display();
+            yield return new WaitForSeconds( gap );
+        }
+
+        this.completed = true;
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    /// <returns>void</returns>
+    private void Init() {
+
+        // set attributes default values.
+        this.completed = false;
+    }
+
+}
