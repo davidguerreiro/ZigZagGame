4d97a72cc6603b780fa2e00f84767334645a94f2
davidguerreiro <me@davidguerreiro.com>
2019-12-27 11:28:04 +0000

    add extra methods to update data in Data panel class

--- UI/StandardLevel/GameOverPanel/DataPanel/DataPanel.cs
+++ UI/StandardLevel/GameOverPanel/DataPanel/DataPanel.cs
@@ -7,6 +7,7 @@ public class DataPanel : MonoBehaviour {
     public AnimationComponent timeWrapper;                  // Time values wrapper animation class component reference.
     public AnimationComponent scoreText;                    // Score text animation class component reference.
     public AnimationComponent score;                        // Score value animation class component reference.
+    public TimeWrapper timeWrapperClass;                    // TimeWrapper logic class component reference.
 
     /// <summary>
     /// Display elements in the game
@@ -25,5 +26,22 @@ public class DataPanel : MonoBehaviour {
         scoreText.Display();
         score.Display();
         yield return new WaitForSeconds( gap );
+    }
+
+    /// <summary>
+    /// Update time and score values.
+    /// </summary>
+    /// <param name="newScore">string - updated score value</param>
+    /// <param name="newHours">string - updated hours value</param>
+    /// <param name="newMinutes">string - updated minutes value</param>
+    /// <param name="newSeconds">string - updated seconds value</param>
+    /// <returns>void</returns>
+    public void UpdateValues( string newScore, string newHours, string newMinutes, string newSeconds ) {
+
+        // update score.
+        score.GetComponent<TextComponent>().UpdateContent( newScore );
+
+        // update hours, miutes and seconds.
+        timeWrapperClass.UpdateTimeValues( newHours, newMinutes, newSeconds );
     }
 }
